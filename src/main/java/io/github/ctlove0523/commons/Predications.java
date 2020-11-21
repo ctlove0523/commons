980d4d70443af163db99d615c2d847922bedaa6a
ctlove0523 <478309639@qq.com>
2020-11-21 21:36:49 +0800

    add method

--- src/main/java/io/github/ctlove0523/commons/Predications.java
+++ src/main/java/io/github/ctlove0523/commons/Predications.java
@@ -1,5 +1,8 @@
 package io.github.ctlove0523.commons;
 
+import java.util.Collection;
+import java.util.Collections;
+
 public class Predications {
 
 	public static String stringNotNull(String str, String message) {
@@ -14,5 +17,21 @@ public class Predications {
 			throw new IllegalArgumentException(message);
 		}
 		return port;
+	}
+
+	public static <T> T notNull(T element, String message) {
+		if (element == null) {
+			throw new IllegalArgumentException(message);
+		}
+
+		return element;
+	}
+
+	public static <T> Collection<T> notEmpty(Collection<T> collection, String message) {
+		if (collection == null || collection.isEmpty()) {
+			throw new IllegalArgumentException(message);
+		}
+
+		return collection;
 	}
 }
